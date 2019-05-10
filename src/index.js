c322a47c9e1ee2f83416e5e006d3cf1efd5d1737
Ivan Sotelo <isotelo@controlla.com.mx>
2019-05-09 22:04:48 -0600

    refactor: change

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import pjson from '../package.json'
 export default {
   install (Vue) {
     Vue.component('vue-cursor', VueCursor)
-    Vue.mixin({
-      created () {
-        // eslint-disable-next-line no-console
-        console.log(`%c Created By Ivan Sotelo %c ${pjson.version} `, 'background: #35495e; color: #fff', 'background: #60b883; color: #fff')
-      }
-    })
   }
 }
 
